6d0dec974e69b41367aba985612499b047d207b3
fh_bots <69167693+fh-bots@users.noreply.github.com>
2020-08-04 02:24:05 +0530

    testf

--- translation.py
+++ translation.py
@@ -6,7 +6,7 @@ This is a Telegram URL Upload Bot!
 
 /help for more details..
 
-by : @filimhouseadmin  ~ @fh_bots
+by : @filimhouseadmin  ~ @fh_bots"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
